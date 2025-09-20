a795f6b80e28ad9a51fd8b13bf22a119f17e249c
Usama Elgendy <usamaaelgendy@gmail.com>
2025-09-20 15:58:37 +0300

    wrap sign-in widget in a column for improved layout structure

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -8,40 +8,36 @@ class AuthScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          SignInScreen(
-            providers: [EmailAuthProvider() , GoogleProvider(clientId: 'project-571836091673')],
-            headerBuilder: (context, constraints, shrinkOffset) {
-              return const Padding(
-                padding: EdgeInsets.all(20),
-                child: AspectRatio(
-                  aspectRatio: 1,
-                  child: Image(image: AssetImage('assets/logo.png')),
-                ),
-              );
-            },
-            subtitleBuilder: (context, action) {
-              return Padding(
-                padding: const EdgeInsets.symmetric(vertical: 8),
-                child: Text(
-                  action == AuthAction.signIn
-                      ? 'Welcome, please sign in!'
-                      : 'Welcome, please sign up!',
-                ),
-              );
-            },
-            footerBuilder: (context, action) {
-              return const Padding(
-                padding: EdgeInsets.only(top: 16),
-                child: Text(
-                  'By signing in, you agree to our T&C.',
-                  style: TextStyle(color: Colors.grey),
-                ),
-              );
-            },
-          ),
-        ],
+      body: SignInScreen(
+        providers: [EmailAuthProvider() , GoogleProvider(clientId: 'project-571836091673')],
+        headerBuilder: (context, constraints, shrinkOffset) {
+          return const Padding(
+            padding: EdgeInsets.all(20),
+            child: AspectRatio(
+              aspectRatio: 1,
+              child: Image(image: AssetImage('assets/logo.png')),
+            ),
+          );
+        },
+        subtitleBuilder: (context, action) {
+          return Padding(
+            padding: const EdgeInsets.symmetric(vertical: 8),
+            child: Text(
+              action == AuthAction.signIn
+                  ? 'Welcome, please sign in!'
+                  : 'Welcome, please sign up!',
+            ),
+          );
+        },
+        footerBuilder: (context, action) {
+          return const Padding(
+            padding: EdgeInsets.only(top: 16),
+            child: Text(
+              'By signing in, you agree to our T&C.',
+              style: TextStyle(color: Colors.grey),
+            ),
+          );
+        },
       ),
     );
   }
